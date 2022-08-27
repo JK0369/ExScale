9ff5a818c44d683c8668b504af28309936c63d12
jake <palatable7@naver.com>
2022-08-27 19:52:40 +0900

    Initial Commit

---
+++ ExScale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExScale
+//
+//  Created by 김종권 on 2022/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
